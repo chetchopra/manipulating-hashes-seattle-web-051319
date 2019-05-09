600a8a1b63d8004c6daf4781074183df48285707
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 21:22:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,8 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  puts contacts["Freddy Mercury"][favorite_icecream_flavors]
+[favorite_icecream_flavors]
 
   return contacts
 end
